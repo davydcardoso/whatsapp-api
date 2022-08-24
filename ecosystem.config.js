ff9103ba7531ffedde85800491483fb1085069e3
Davyd Cardoso <davydkewen@gmail.com>
2022-08-23 23:18:14 -0300

    fix: correction ecosystem pm2 tasks

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,12 @@ module.exports = {
   apps: [
     {
       name: "RocketApps:WAAPI-HTTPS",
-      script: "node ./dist/infra/http/server.ts",
+      script: "node ./dist/infra/http/server.js",
       max_memory_restart: "450M",
     },
     {
       name: "RocketApps:WAAPI-WORKERS",
-      script: "node ./dist/infra/queue/worker.ts",
+      script: "node ./dist/infra/queue/worker.js",
       max_memory_restart: "450M",
     },
   ],
